1fb4f13a732d41bae8cec1dce1a519e08b7bb183
kim-hc <joel9537@gmail.com>
2022-01-25 14:49:37 +0900

    .

--- script/gain_tuner.py
+++ script/gain_tuner.py
@@ -6,12 +6,18 @@ wn = 100
 #      under < 1
 #       over > 1
 damped = 1.0
+og_damped = 0.057735027
 
 kp = wn * wn
 kv = int(2.0 * damped * wn)
+og_kp = wn * wn
+og_kv = int(2.0 * og_damped * wn)
 
 print('kp:',kp)
 print('kv:',kv)
+print('og_kp:',og_kp)
+print('og_kv:',og_kv)
+print('')
 
 # original gain -> damping_ratio = 0.057735026
 # print(math.sqrt(1200))
